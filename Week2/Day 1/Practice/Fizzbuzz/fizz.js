3f72b6f5c03a6064aaecb44a5df986ecbee3688a
youssefch2003 <medyoussef429@gmail.com>
2023-05-08 11:19:14 +0100

    Fizzbuzz

---
+++ Week2/Day 1/Practice/Fizzbuzz/fizz.js
@@ -0,0 +1,8 @@
+    for(var i=1 ; i<101; i++){
+            if(i%3==0 || i%5==0 || (i%5==0 && i%3==0)){
+                console.log("Fizz");
+
+            }else{
+                console.log(i);
+            }
+        }
