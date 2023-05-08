2fb99d346fcda006fcb6ed2e41ef09b6548a6159
youssefch2003 <medyoussef429@gmail.com>
2023-05-08 11:56:11 +0100

    Loop Challenges

---
+++ Week2/Day 1/Practice/Loop Challenges/loop.js
@@ -0,0 +1,32 @@
+// 1
+var i=1;
+while(i<20){
+    if(i%2!=0){
+        console.log(i);
+    }
+    i++;
+}
+// 2
+for(var y=100;y>0;y--){
+    if(y%3==0){
+        console.log(y);
+    }
+}
+// 3
+for(var x=4; x>-4;x=x-1.5){
+    console.log(x);
+
+}
+// 4
+var sum=0;
+for(var j=1;j<101;j++){
+    sum+=j;
+}
+console.log(sum);
+// 5
+var f=1;
+for(var w=2;w<13;w++){
+     f=f*w;
+
+}
+console.log(f);
