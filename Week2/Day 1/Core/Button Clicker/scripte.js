a567f7aa4099358583a2cd075bf35964f1cf0fe8
youssefch2003 <medyoussef429@gmail.com>
2023-05-10 12:06:45 +0100

    button clicker

---
+++ Week2/Day 1/Core/Button Clicker/scripte.js
@@ -0,0 +1,16 @@
+var you = document.querySelector("#yo");
+
+function change(){
+    alert("ninja was liked");
+
+}
+function logout(elm) {
+    elm.innerText = "Logout";
+}
+
+function rem(elm){
+    elm.remove();
+   
+   
+    
+}
