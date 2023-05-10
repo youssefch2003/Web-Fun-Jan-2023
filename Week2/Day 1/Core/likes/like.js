7e8f75a87da3185c3e431d20cc3753ef4dccae17
youssefch2003 <medyoussef429@gmail.com>
2023-05-10 20:58:43 +0100

    likes

---
+++ Week2/Day 1/Core/likes/like.js
@@ -0,0 +1,15 @@
+var count = 0;
+function counterFunc(){
+    count++;
+    document.querySelector(".num").innerHTML = count;
+}
+var count0 = 0;
+function counterFunc1(){
+    count0++;
+    document.querySelector(".num1").innerHTML = count0;
+}
+var count1= 0;
+function counterFunc2(){
+    count1++;
+    document.querySelector(".num2").innerHTML = count1;
+}
---
+++ Week2/Day 1/Core/likes/script.js
@@ -0,0 +1,5 @@
+var count = 0;
+function counterFunc(){
+    count++;
+    document.querySelector(".num").innerHTML = count;
+}
