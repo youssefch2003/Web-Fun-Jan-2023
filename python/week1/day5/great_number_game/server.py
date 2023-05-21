7b0b9cc70499a9eeb8209ae8846acae72a86b14f
youssefch2003 <medyoussef429@gmail.com>
2023-05-21 22:34:17 +0100

    great_number_game

---
+++ python/week1/day5/great_number_game/server.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,redirect,session, request
+import random
+app=Flask(__name__)
+app.secret_key = "lalalalalalal"
+
+@app.route('/')
+def hello():
+    if  'num' not in session :
+        session['num']= random.randint(1, 100) 
+    if 'choice' not in session :
+        session['choice'] = 0
+    return render_template('index.html')
+
+@app.route('/check',methods=['post'])
+def check():
+    session['choice'] = int( request.form['ran'])s
+        
+    return redirect('/')
+
+@app.route('/res')
+def res():
+    session.clear()
+    
+    return redirect('/')
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
