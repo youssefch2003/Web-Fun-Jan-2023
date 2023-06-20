afcfb905ffd100be4415f2d7d457b70caa72aec1
youssefch2003 <medyoussef429@gmail.com>
2023-06-20 12:50:36 +0100

    superninja

--- js/day2/superninja.js
+++ js/day2/superninja.js
@@ -21,12 +21,17 @@ class Ninja {
 class Sensei extends Ninja {
     constructor(name){
         super(name,200,10,10)
-        this.wisdom = 10
+        this.wisdom = "What one programmer can do in one month, two programmers can do in two months."
     }
     speakWisdom(){
         this.drinkSake();
-        console.log("What one programmer can do in one month, two programmers can do in two months.");
+        console.log(this.wisdom);
     }
+    showStats(){
+        const {wisdom,...senseiwithoutwisdom} = this
+        console.log(senseiwithoutwisdom);
+    }
+
     
 }
 const superSensei = new Sensei("Jackie Chan");
