37a3194ab2e71561b7aed6de9b36493c693ec5b8
youssefch2003 <medyoussef429@gmail.com>
2023-06-21 19:15:10 +0100

    first jsx

---
+++ react/day1/first_jsx/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Dojo !</h1>
+    <h2>Things i need to do:</h2>
+    <ul><li>learn react</li>
+    <li>climb mt everest</li>
+    <li>feed the dog</li></ul>
+    </div>
+  );
+}
+
+export default App;
