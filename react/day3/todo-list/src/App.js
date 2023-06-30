3d8abf9002b7946b870888ff8821af9388ccf9f6
youssefch2003 <medyoussef429@gmail.com>
2023-06-30 19:18:50 +0100

    TODO LIST

---
+++ react/day3/todo-list/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Todo from './components/todo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Todo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
